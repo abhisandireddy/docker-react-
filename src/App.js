852c4529e6505f79c5d188faeba3eb67e79bee9f
Abhi Sandire <abhilash.sandireddy@gmail.com>
2020-09-27 18:52:06 -0500

    latest build changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Caesar is the best dog ever! --By Abhi
+          Edit <code>src/App.js</code> and save to reload. Caesar is the best dog ever!!! --By Abhi
         </p>
         <a
           className="App-link"
